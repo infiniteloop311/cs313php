663ec818331dd6f7255e8b43b18c98ec64f51563
Joshua <infiniteloop311@gmail.com>
2019-05-01 15:06:47 -0400

    finished the core requirements

--- web/02ta.js
+++ web/02ta.js
@@ -3,3 +3,10 @@ function buttonIsClicked() {
     "use strict";
     alert("Clicked!");
 }
+
+function changeColor() {
+    "use strict";
+    var color;
+    color = document.getElementById("change").value;
+    document.getElementById("div1").style.backgroundColor = color;
+}
