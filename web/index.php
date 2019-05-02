e7e76c90b12aa9303c8471c7cb63c5f2c1948ec4
Joshua <infiniteloop311@gmail.com>
2019-05-02 14:36:28 -0400

    Testing css file included in my php file

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@
     <title>02 - Prove</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="03prove.css">
+    <link rel="stylesheet" type="text/css" href="02prove.css">
 </head>
 <body>
     <h1>Home Page</h1>
