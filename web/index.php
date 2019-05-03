ec90765f904f9802da2e61fed0d609cb4800bcd5
Joshua <infiniteloop311@gmail.com>
2019-05-03 11:26:49 -0400

    changes

--- web/index.php
+++ web/index.php
@@ -22,8 +22,8 @@
             I took Web Engineering I last semester because I wanted to get some experience with web development and as it turns out I really enjoyed it. Since I don't have a lot of space my last semester, I'm taking this class now so that I can delve deeper into web development server side programming. I'm hoping that it'll be as fun as the prerequisite class and that I'll learn a lot. 
         </p>
     </div>
+    <button id="tbutton">Click here for some inspirational quotes!</button>
     <div id="quotes">
-        <button id="tbutton">Click here for some inspirational quotes!</button>
         <p>
             "I am a slow walker, but I never walk back." - Abraham Lincoln
         </p>
