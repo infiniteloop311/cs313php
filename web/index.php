f54082a394b21c7925d6c846f359c8471faab10f
Joshua <infiniteloop311@gmail.com>
2019-05-03 11:45:06 -0400

    some final changes

--- web/index.php
+++ web/index.php
@@ -36,8 +36,9 @@
         <q>
             To say that nothing is true is to realize that the foundations of society are fragile and that we must be the shepherds of our civilization. To say that everything is permitted is to understand that we are the architects of our actions, and that we must live with their consequences, whether glorious or tragic. - Anonymous
         </q>
-        <br/><br/>
+        <br/>
     </div>
+    <br/>
     <?php echo date("D M d, Y G:i a"); ?>
 </body>
 </html>
