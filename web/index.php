108cf59a19026c7c9a442a29aae7d0666382a33e
Joshua <infiniteloop311@gmail.com>
2019-05-03 11:31:35 -0400

    included script file in php file

--- web/index.php
+++ web/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="02prove.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="02prove.js"></script>
 </head>
 <body>
     <div id="header">
