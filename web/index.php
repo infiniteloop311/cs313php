642abdd364ca7cc157022c76009a45220bf8a3e3
Joshua <infiniteloop311@gmail.com>
2019-05-03 11:37:24 -0400

    added quotes

--- web/index.php
+++ web/index.php
@@ -28,7 +28,14 @@
         <p>
             "I am a slow walker, but I never walk back." - Abraham Lincoln
         </p>
+        <p>
+            "Keep your eyes on the stars, and your feet on the ground." - Theodore Roosevelt
+        </p>
+        <p>
+            “To say that nothing is true is to realize that the foundations of society are fragile and that we must be the shepherds of our civilization. To say that everything is permitted is to understand that we are the architects of our actions, and that we must live with their consequences, whether glorious or tragic.” - Anonymous
+        </p>
     </div>
+    <br/>
     <?php echo date("D M d, Y G:i a"); ?>
 </body>
 </html>
