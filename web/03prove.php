cd192f7f36005b835a69014ea73731257b2d79d2
Joshua <infiniteloop311@gmail.com>
2019-05-10 17:25:32 -0400

    testing some button onclick functions to execute php

--- web/03prove.php
+++ web/03prove.php
@@ -9,12 +9,18 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-    <button onclick="<?php echo 'Clicked!'?>">Add session variable.</button><br/>
+    <a href="03prove.php?variable=1">Click me.</a><br/>
     <?php
+    /*
     $_SESSION["favcolor"] = "green";
     $_SESSION["favanimal"] = "cat";
     echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
     echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+    */
+    if (isset($_GET['variable'])) {
+        $_SESSION["item"] = "bought";
+    }
+    print_r($_SESSION);
     ?>
 </body>
 </html>
