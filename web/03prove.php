cc5d46b7358420b39833510400517133a2dde59c
Joshua <infiniteloop311@gmail.com>
2019-05-10 17:53:17 -0400

    testing some links to execute php

--- web/03prove.php
+++ web/03prove.php
@@ -9,10 +9,22 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-    <a href="03prove.php?item1=1">Click me.</a><br/>
+    <a href="03prove.php?item1=bought">Item 1</a><br/>
+    <a href="03prove.php?item2=bought">Item 2</a><br/>
+    <a href="03prove.php?item3=bought">Item 3</a><br/>
+    <a href="03prove.php?item4=bought">Item 4</a><br/>
     <?php
     if (isset($_GET['item1'])) {
-        $_SESSION["item"] = "bought";
+        $_SESSION["item1"] = "bought";
+    }
+    if (isset($_GET['item2'])) {
+        $_SESSION["item2"] = "bought";
+    }
+    if (isset($_GET['item3'])) {
+        $_SESSION["item3"] = "bought";
+    }
+    if (isset($_GET['item4'])) {
+        $_SESSION["item4"] = "bought";
     }
     print_r($_SESSION);
     ?>
