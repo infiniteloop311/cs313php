40d4a9d2402b9c99469142972c4b58cc48abfc90
Joshua <infiniteloop311@gmail.com>
2019-05-11 18:36:46 -0400

    changing styles

--- web/03proveCheckout.php
+++ web/03proveCheckout.php
@@ -7,22 +7,25 @@ session_start();
     <title>Checkout</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="03prove.css">
 </head>
 <body>
-    <h1>Checkout</h1>
-    <form method="post" action="03proveConfirmation.php">
-        <span>Street: </span>
-        <input type="text" name="Street"><br/>
-        <span>City: </span>
-        <input type="text" name="City"><br/>
-        <span>State: </span>
-        <input type="text" name="State" maxlength="2" size="2"><br/>
-        <span>Zipcode: </span>
-        <input type="text" name="Zipcode" maxlength="5" size="5"><br/>
-        <input type="submit" value="Confirm Purchase">
-    </form>
-    <br/>
-    <br/>
-    <a href="03proveCart.php">Back To Cart</a>
+    <div id="main2">
+        <h1>Checkout</h1>
+        <form method="post" action="03proveConfirmation.php">
+            <span>Street: </span>
+            <input type="text" name="Street"><br/>
+            <span>City: </span>
+            <input type="text" name="City"><br/>
+            <span>State: </span>
+            <input type="text" name="State" maxlength="2" size="2"><br/>
+            <span>Zipcode: </span>
+            <input type="text" name="Zipcode" maxlength="5" size="5"><br/>
+            <input type="submit" value="Confirm Purchase">
+        </form>
+        <br/>
+        <br/>
+        <a href="03proveCart.php">Back To Cart</a>
+    </div>
 </body>
 </html>
