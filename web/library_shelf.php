0f472b833280f1b0d2332dd1b1fdae0bec516c91
Joshua <infiniteloop311@gmail.com>
2019-05-24 14:43:32 -0400

    debug changes to access database from the same file and not an external file

--- web/library_shelf.php
+++ web/library_shelf.php
@@ -1,6 +1,28 @@
 <?php
-require "dbConnect.php";
-$db = get_db();
+//require "dbConnect.php";
+//$db = get_db();
+
+try
+{
+    $dbUrl = getenv('DATABASE_URL');
+
+    dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+}
 
 foreach ($db->query('SELECT books.title, authorsinfo.name FROM library
                      INNER JOIN books ON library.book_id=books.id
