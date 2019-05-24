022bbcf8505565a4a9dbae4d6b5a8efa38728f57
Joshua <infiniteloop311@gmail.com>
2019-05-24 17:41:27 -0400

    added a break

--- web/library_shelf.php
+++ web/library_shelf.php
@@ -2,16 +2,11 @@
 require "dbConnect.php";
 $db = getDB();
 
-foreach ($db->query('SELECT * FROM shelf') as $row)
-{
-    echo $row['user_id'] . " " . $row['book_id'] . " " . $row['author_id'] . "<br>";
-}
-
 foreach ($db->query('SELECT books.title, authorsinfo.name FROM shelf
                      INNER JOIN books ON shelf.book_id=books.id
                      INNER JOIN authorsinfo ON shelf.author_id=authorsinfo.id') as $row)
 {
-    echo $row['title'] . " by " . $row['name'];
+    echo $row['title'] . " by " . $row['name'] . "<br/>";
 }
 ?>
 
