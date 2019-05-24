6b3fdd94cf507180387881efcfcb110f2585a8f6
Joshua <infiniteloop311@gmail.com>
2019-05-24 17:33:18 -0400

    debugging

--- web/library_shelf.php
+++ web/library_shelf.php
@@ -23,17 +23,15 @@ catch (PDOException $ex)
 
 foreach ($db->query('SELECT * FROM shelf') as $row)
 {
-    echo $row['user_id'] . " " . $row['book_id'] . ":" . $row['author_id'] . "<br>";
+    echo $row['user_id'] . " " . $row['book_id'] . " " . $row['author_id'] . "<br>";
 }
 
-/*
 foreach ($db->query('SELECT books.title, authorsinfo.name FROM library
                      INNER JOIN books ON library.book_id=books.id
                      INNER JOIN authorsinfo ON library.author_id=authorsinfo.id') as $row)
 {
     echo $row['title'] . " by " . $row['name'];
 }
-*/
 ?>
 
 <!DOCTYPE html>
