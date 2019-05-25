9f8574a52a268810b5422a28b5c1a51c5ccc0ab4
Joshua <infiniteloop311@gmail.com>
2019-05-25 01:19:34 -0400

    testing displaying info from books and authorsinfo tables in their respective pages

--- web/library_author.php
+++ web/library_author.php
@@ -3,4 +3,14 @@ require "dbConnect.php";
 $db = getDB();
 
 echo 'Author Page';
+
+if (isset($_GET['author_id']))
+{
+    $id = $_GET['author_id'];
+
+    foreach ($db->query("SELECT * FROM authorsinfo WHERE id='$id'") as $row)
+    {
+        echo $row['name'];
+    }
+}
 ?>
--- web/library_book.php
+++ web/library_book.php
@@ -3,4 +3,14 @@ require "dbConnect.php";
 $db = getDB();
 
 echo 'Book Page';
+
+if (isset($_GET['book_id']))
+{
+    $id = $_GET['book_id'];
+
+    foreach ($db->query("SELECT * FROM books WHERE id='$id'") as $row)
+    {
+        echo $row['title'];
+    }
+}
 ?>
