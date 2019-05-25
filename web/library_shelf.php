2ba5a26cb8c19527981879299ca480e6c7acaf92
Joshua <infiniteloop311@gmail.com>
2019-05-24 22:27:52 -0400

    adding the cover to the output from the database

--- web/library_shelf.php
+++ web/library_shelf.php
@@ -6,9 +6,8 @@ foreach ($db->query('SELECT books.title, authorsinfo.name FROM shelf
                      INNER JOIN books ON shelf.book_id=books.id
                      INNER JOIN authorsinfo ON shelf.author_id=authorsinfo.id') as $row)
 {
-    $cover = $row['cover'];
-    echo $cover . "<br/>";
-    echo $row['cover']/*"<img src=\"$cover\" alt=book_cover>"*/ . "<br/>" . $row['title'] . " by " . $row['name'] . "<br/><br/>";
+    echo $row['cover'] . "<br/>" . $row['title'] . " by " . $row['name'] . "<br/><br/>";
+    /*"<img src=\"$cover\" alt=book_cover>"*/
 }
 ?>
 
