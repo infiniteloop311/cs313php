f568963beacb3385929f0b2d79a701f2c7df59e4
Joshua <infiniteloop311@gmail.com>
2019-05-30 15:40:15 -0400

    testing div columns

--- web/library_shelf.php
+++ web/library_shelf.php
@@ -11,6 +11,7 @@ $db = getDB();
             img {
                 width: 15%;
             }
+            .col-4 {width: 33.33%;}
         </style>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
@@ -23,21 +24,26 @@ $db = getDB();
         <header>
             <a href="library_search.php">Search</a><br/><br/>
         </header>
-        <?php
-        foreach ($db->query("SELECT shelf.book_id, shelf.author_id, books.cover, books.title, authorsinfo.name 
-                            FROM shelf
-                            INNER JOIN books ON shelf.book_id=books.id
-                            INNER JOIN authorsinfo ON shelf.author_id=authorsinfo.id") as $row)
-        {
-            $bookid = $row['book_id'];
-            $authorid = $row['author_id'];
-            $title = $row['title'];
-            $name = $row['name'];
-            $cover = $row['cover'];
-            echo "<img src=\"$cover\" alt=$cover><br/>" . 
-                "<a href='library_book.php?book_id=$bookid'>$title</a>" . 
-                "<br/>by " . "<a href='library_author.php?author_id=$authorid'>$name</a>" . "<br/><br/>";
-        }
-        ?>
+        <main>
+            <?php
+            foreach ($db->query("SELECT shelf.book_id, shelf.author_id, books.cover, books.title, authorsinfo.name 
+                                FROM shelf
+                                INNER JOIN books ON shelf.book_id=books.id
+                                INNER JOIN authorsinfo ON shelf.author_id=authorsinfo.id") as $row)
+            {
+                $bookid = $row['book_id'];
+                $authorid = $row['author_id'];
+                $title = $row['title'];
+                $name = $row['name'];
+                $cover = $row['cover'];
+                echo "<img src=\"$cover\" alt=$cover><br/>" . 
+                    "<a href='library_book.php?book_id=$bookid'>$title</a>" . 
+                    "<br/>by " . "<a href='library_author.php?author_id=$authorid'>$name</a>" . "<br/><br/>";
+            }
+            ?>
+            <div class="col-4">One</div>
+            <div class="col-4">Two</div>
+            <div class="col-4">Three</div>
+        </main>
     </body>
 </html>
