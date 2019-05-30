3b81b6f75c16f2f2877ef44cc673df8e0d4debaa
Joshua <infiniteloop311@gmail.com>
2019-05-30 16:10:04 -0400

    adding div columns to php output

--- web/library_search.php
+++ web/library_search.php
@@ -45,9 +45,9 @@ $db = getDB();
                     $cover = $row['cover'];
                     $title = $row['title'];
                     $name = $row['name'];
-                    echo "<img src=\"$cover\" alt=$cover><br/>" . 
+                    echo "<div class=\"col-4\"><img src=\"$cover\" alt=$cover><br/>" . 
                         "<a href='library_book.php?book_id=$bookid'>$title</a>" . 
-                        "<br/>by " . "<a href='library_author.php?author_id=$authorid'>$name</a>" . "<br/><br/>";
+                        "<br/>by " . "<a href='library_author.php?author_id=$authorid'>$name</a></div>";
                 }
             }            
         }
