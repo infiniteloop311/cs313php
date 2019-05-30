11add9f4a6a1a09a415a8653a8255b360b2edc8e
Joshua <infiniteloop311@gmail.com>
2019-05-30 18:24:27 -0400

    adding div columns to php output

--- web/library_shelf.php
+++ web/library_shelf.php
@@ -37,7 +37,7 @@ $db = getDB();
                     "<br/>by " . "<a href='library_author.php?author_id=$authorid'>$name</a></div>";
             }
             ?>
-            <br/>
+            <br/><br/>
             <div class="col-4">Test</div>
         </main>
     </body>
