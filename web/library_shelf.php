87c9d04aa6f3f0e1bc0d681833067d4be9dde17f
Joshua <infiniteloop311@gmail.com>
2019-06-01 23:34:54 -0400

    changing styles

--- web/library_shelf.php
+++ web/library_shelf.php
@@ -38,7 +38,7 @@ $db = getDB();
                 <a href="library_search.php">Search</a><br/><br/>
             </div>
         </header>
-        <main style="margin: 0px, border: none">
+        <main style="margin: 0px; border: 0px">
             <?php
             $id = $_SESSION["userid"];
             foreach ($db->query("SELECT shelf.user_id, shelf.book_id, shelf.author_id, books.cover, books.title, authorsinfo.name 
